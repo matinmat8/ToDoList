d763b853fa555a4b3e399e1e26fb7539389537f2
MatinMat8 <matinmat8.mat@gmail.com>
2021-11-13 14:29:09 +0330

    set up costum admin panel

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ToDoList
 
-# Register your models here.
+
+@admin.register(ToDoList)
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'due_date', 'reminder',)
+    search_fields = ('title', 'description', 'user', 'due_date', 'create_at')
+    list_filter = ('user', 'create_at')
+    prepopulated_fields = {'slug': ('title', 'description','priority')}
