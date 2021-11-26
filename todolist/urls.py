07b76dd62ae3accf78efc4b0ce0ce574285fb3a4
MatinMat8 <matinmat8.mat@gmail.com>
2021-11-26 22:40:44 +0330

    added url for call FilterView

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WorksList
+from .views import WorksList, FilterView
 
 app_name = 'todolist'
 
 urlpatterns = [
     path('list/', WorksList.as_view(), name='list'),
+    path('filter/', FilterView.as_view(), name='filter')
 ]
