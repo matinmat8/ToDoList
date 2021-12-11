0dd074ed1bd435aefdd84c408eddf80cbca8d251
MatinMat8 <matinmat8.mat@gmail.com>
2021-12-11 18:21:42 +0330

    handle admin page for Comment's model

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ToDoList
+from .models import ToDoList, Comment
 
 
 @admin.register(ToDoList)
@@ -8,3 +8,10 @@ class ToDoListAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description', 'user', 'due_date', 'create_at')
     list_filter = ('user', 'create_at')
     prepopulated_fields = {'slug': ('title', 'description','priority')}
+
+
+@admin.register(Comment)
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'for_work',)
+    search_fields = ('user', 'for_work')
+    list_filter = ('user', 'for_work')
