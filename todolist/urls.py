0a58506ba704556c87dd5fa53bb8ad3a8e46e77c
MatinMat8 <matinmat8.mat@gmail.com>
2021-12-12 02:39:15 +0330

    add path to handle add comment

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import WorksList, FilterView, GetChildren, index
+from .views import WorksList, FilterView, GetChildren, index, AddComment
 
 app_name = 'todolist'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('list/', WorksList.as_view(), name='list'),
     path('filter/', FilterView.as_view(), name='filter'),
-    path('list/children/<int:pk>/', GetChildren.as_view(), name='get_children')
+    path('list/children/<int:pk>/', GetChildren.as_view(), name='get_children'),
+    path('add_comment/<int:pk>/', AddComment.as_view(), name='add_comment')
 ]
