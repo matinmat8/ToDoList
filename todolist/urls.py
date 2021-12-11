f0618ef5f8f23ce4d9d26e78a7c214239d067bab
MatinMat8 <matinmat8.mat@gmail.com>
2021-12-11 23:30:56 +0330

    added a path for index view

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import WorksList, FilterView, GetChildren
+from .views import WorksList, FilterView, GetChildren, index
 
 app_name = 'todolist'
 
 urlpatterns = [
+    path('', index, name='index'),
     path('list/', WorksList.as_view(), name='list'),
     path('filter/', FilterView.as_view(), name='filter'),
     path('list/children/<int:pk>/', GetChildren.as_view(), name='get_children')
