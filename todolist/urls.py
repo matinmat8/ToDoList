204055ba0e1846fc96eb0ccba22f8945520ae9b7
MatinMat8 <matinmat8.mat@gmail.com>
2021-12-11 18:28:52 +0330

    added a path for get children

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import WorksList, FilterView
+from .views import WorksList, FilterView, GetChildren
 
 app_name = 'todolist'
 
 urlpatterns = [
     path('list/', WorksList.as_view(), name='list'),
-    path('filter/', FilterView.as_view(), name='filter')
+    path('filter/', FilterView.as_view(), name='filter'),
+    path('list/children/<int:pk>/', GetChildren.as_view(), name='get_children')
 ]
