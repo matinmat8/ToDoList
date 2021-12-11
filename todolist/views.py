0ba6e65dad5132e6db5d7972f71327b5e85d3f80
MatinMat8 <matinmat8.mat@gmail.com>
2021-12-11 18:33:12 +0330

    change queryset for filter just parents

--- todolist/views.py
+++ todolist/views.py
@@ -20,7 +20,7 @@ class WorksList(ListView, LoginRequiredMixin):
     def get_queryset(self):
         obj = super().get_queryset()
         today = datetime.date.today()
-        return obj.filter(user=self.request.user, due_date=today)
+        return obj.filter(user=self.request.user, due_date=today, tree=None)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         object_list = super().get_context_data(**kwargs)
