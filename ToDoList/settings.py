de5866b0e2f923bb1d17bdf76a238c2f1e60e984
MatinMat8 <matinmat8.mat@gmail.com>
2022-02-28 17:36:14 +0330

    changing DB to sqlite

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -84,17 +84,23 @@ WSGI_APPLICATION = 'ToDoList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'todolist',
+#         'USER': 'matinmat88',
+#         'PASSWORD': 'IF MatinMat8',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todolist',
-        'USER': 'matinmat88',
-        'PASSWORD': 'IF MatinMat8',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
