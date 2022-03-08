e5b797dda33a10f712ea0925e7de2ea437ba04e9
MatinMat8 <matinmat8.mat@gmail.com>
2022-03-08 23:59:26 +0330

    testing work list performance

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .views import WorksList
+
+
+class TestingWorkList(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='Matinmat88', password='aliMatinMat89')
+
+    def testing_work_list(self):
+        request = self.factory.get('list')
+        request.user = self.user
+        response = WorksList.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.user, self.user)
+
